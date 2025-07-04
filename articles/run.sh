98ff18f606bba2ff579e6103cfac29b72c317882
madelyne <madelyne.xiao96@gmail.com>
2025-07-04 08:26:32 -0400

    update run.sh

--- articles/run.sh
+++ articles/run.sh
@@ -11,8 +11,8 @@ seeds=(384 328 479 21 304 355 285 105 135 263 91 88 73 177 7 66 492 344 402 274 
 for seed in "${seeds[@]}";
 do
   echo "Running with random seed: $seed"
-  # python CNN_RNN.py --test_source isot --rand_seed "$seed" >> $OUTPUT_FILE_ISOT &
-  # python CNN_RNN.py --test_source fakes --rand_seed "$seed" >> $OUTPUT_FILE_FAKES &
+  python CNN_RNN.py --test_source isot --rand_seed "$seed" >> $OUTPUT_FILE_ISOT &
+  python CNN_RNN.py --test_source fakes --rand_seed "$seed" >> $OUTPUT_FILE_FAKES &
   
   python CNN_revised.py --test_source nytimes --rand_seed "$seed" >> $OUTPUT_FILE_NYT &
   python CNN_revised.py --test_source reuters --rand_seed "$seed" >> $OUTPUT_FILE_REU &
